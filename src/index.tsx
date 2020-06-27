a59eecb309f32c8ef4694c65bfac86c2b9859835
Matheus Gambati <matheusgambati@gmail.com>
2020-06-27 16:31:06 -0400

    Adiciona os providers do react-router e do theme-ui

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { store } from './app/store';
-import { Provider } from 'react-redux';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider, Theme } from "theme-ui";
+import App from "./App";
+import { store } from "./app/store";
+import * as serviceWorker from "./serviceWorker";
+import { BASE_THEME } from "./app/constants/BASE_THEME";
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <ThemeProvider theme={BASE_THEME as Theme}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ThemeProvider>
     </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
