659bd0e73292a349d275e749770adbfe15630b78
Matheus Gambati <matheusgambati@gmail.com>
2020-06-28 19:12:34 -0400

    Altera field "description" de Serie para ser optional

--- src/features/series/typings/seriesTypes.ts
+++ src/features/series/typings/seriesTypes.ts
@@ -3,6 +3,6 @@ import { ImageResource } from "../../../app/typings/apiTypes";
 export interface Series {
   id: number;
   title: string;
-  description: string;
+  description?: string;
   thumbnail: ImageResource;
 }
