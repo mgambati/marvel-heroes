653d12991d78cc40b7396d48cb33af46af8a10cd
Matheus Gambati <matheusgambati@gmail.com>
2020-06-28 16:59:09 -0400

    Altera testes de CharatersSearch e CharacterList para usar MSW
    Melhora consideravelmente a confiabilidade dos testes pois com MSW é possível testar todo fluxo até chegar na request.

--- src/features/characters/components/CharacterSearch.test.tsx
+++ src/features/characters/components/CharacterSearch.test.tsx
@@ -2,11 +2,17 @@ import React from "react";
 import CharacterSearch from "./CharacterSearch";
 import * as charactersThunks from "../thunks/charactersThunks";
 import { render, fireEvent, waitFor } from "../../../app/helpers/test-utils";
+import CharactersList from "./CharactersList";
 
 test("should dispatch loadCharacters on input change", async () => {
   jest.spyOn(charactersThunks, "loadCharacters");
 
-  const { getByTestId } = render(<CharacterSearch />);
+  const { getByTestId, findByText } = render(
+    <>
+      <CharacterSearch />
+      <CharactersList />
+    </>
+  );
 
   const input = getByTestId("search-input");
   fireEvent.change(input, { target: { value: "loki" } });
@@ -17,6 +23,8 @@ test("should dispatch loadCharacters on input change", async () => {
       offset: 0,
     });
   });
+
+  expect(await findByText("Loki")).toBeInTheDocument();
 });
 
 test("should dispatch loadCharacters with query 'undefined' when input is empty", async () => {
--- src/features/characters/components/CharactersList.test.tsx
+++ src/features/characters/components/CharactersList.test.tsx
@@ -1,40 +1,36 @@
 import React from "react";
-import { fireEvent, render, waitFor } from "../../../app/helpers/test-utils";
+import { fireEvent, render } from "../../../app/helpers/test-utils";
+import { store } from "../../../app/store";
 import * as charactersApi from "../api/charactersApi";
+import { loadCharacters } from "../thunks/charactersThunks";
 import CharactersList from "./CharactersList";
-import * as charactersSelectors from "../selectors/charactersSelectors";
-import { CHARACTERS_RESPONSE_PAYLOAD } from "../api/__fixtures__/charactersRequests";
 
-beforeEach(() => {
-  jest
-    .spyOn(charactersSelectors, "selectAllCharactersIds")
-    .mockReturnValue(
-      CHARACTERS_RESPONSE_PAYLOAD.results.map((item) => item.id)
-    );
-
-  jest
-    .spyOn(charactersApi, "fetchCharacters")
-    .mockResolvedValueOnce(CHARACTERS_RESPONSE_PAYLOAD)
-    .mockResolvedValueOnce(CHARACTERS_RESPONSE_PAYLOAD);
+beforeAll(() => {
+  jest.spyOn(charactersApi, "fetchCharacters");
 });
 
-beforeEach(() => {
-  jest.clearAllMocks();
+test("renders a loading spinner when pending", async () => {
+  const { findByTestId } = render(<CharactersList />);
+  store.dispatch(loadCharacters({ offset: 0 }));
+  expect(await findByTestId("loading-characters-spinner")).toBeInTheDocument();
 });
 
 test("renders a list of CharactersCard", async () => {
+  await store.dispatch(loadCharacters({ offset: 0, query: undefined }));
+
   const { findAllByTestId } = render(<CharactersList />);
 
   expect(await findAllByTestId("character-card")).toHaveLength(2);
 });
 
 test("loads more characters", async () => {
-  const { getByTestId } = render(<CharactersList />);
+  const { findByTestId, getByTestId } = render(<CharactersList />);
 
   const loadMoreButton = getByTestId("load-more-btn");
   fireEvent.click(loadMoreButton);
 
-  await waitFor(() => {
-    expect(charactersApi.fetchCharacters).toHaveBeenCalledTimes(1);
-  });
+  expect(await findByTestId("loading-characters-spinner")).toBeInTheDocument();
+
+  expect(await findByTestId("load-more-btn")).toBeInTheDocument();
+  expect(charactersApi.fetchCharacters).toHaveBeenCalledWith(20, undefined);
 });
