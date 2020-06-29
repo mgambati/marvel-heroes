12da061ce96eb2ddf1d18739d1497af683d55a3d
mgambati <mgambati@users.noreply.github.com>
2020-06-29 01:01:26 +0000

    deploy: 4b979b1172e7973e7f12882ee7bb23fb835bfb94

---
+++ precache-manifest.f3eecddfed5f634d6e4c6c7e26f34f96.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef6c3fe581486b3727e41f70cec2aa5c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b371b64a081d83fa54f9",
+    "url": "/static/js/2.6efb9c00.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/static/js/2.6efb9c00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7433aaeab4e4b64c95ef",
+    "url": "/static/js/main.ddebb744.chunk.js"
+  },
+  {
+    "revision": "e7614f69dfb5d5a07f1c",
+    "url": "/static/js/runtime-main.b1c5e7c0.js"
+  },
+  {
+    "revision": "f19e3c78fb89262e613275e2ec94412a",
+    "url": "/static/media/logo.f19e3c78.png"
+  }
+]);
